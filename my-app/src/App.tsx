af25510ee141fd96b86ac142ec374128d00df11a
Piskun Ivan <tw1st3r111@mail.ru>
2023-11-30 00:21:43 +0300

    task7

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -26,7 +26,7 @@ function DateTimePretty({ dateTime, date }: propTimePrettyT) {
   )} лет назад`;
   let f1, f2;
   // f1 = moment().diff(currentTime, 'hour', true);
-  f2 = moment(currentTime).diff(date, 'hour', true);
+  f2 = moment(currentTime).diff(date, 'hour', true); // тут какой-то бред
   const f3 = moment(f2).minutes();
   console.log(f3);
   console.log(currentTime, date);
